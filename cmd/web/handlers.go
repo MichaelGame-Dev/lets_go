964944e982d0f7a9657f43fada0f592169dc7160
Game Dev With Michael <44328815+MichaelGame-Dev@users.noreply.github.com>
2024-08-05 12:50:48 -0500

    Updates from templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,23 +10,39 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
+
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	// at first, it threw me off that I had to add partials. But then I realized the template file has no idea where partials are
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
 	// Use the template.ParseFiles() function to read the template file into a
 	// template set. If there's an error, we log the detailed error message, use
 	// the http.Error() function to send an Internal Server Error response to the
 	// user, and then return from the handler so no subsequent code is executed.
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	// We pass the files to ParseFiles() in a set. Notice we use ... to pass the
+	// contents of the files slice as variadic arguements.
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	// Then we use the Execute() method on the template set to write the
+	// OLD: Then we use the Execute() method on the template set to write the
 	// template content as the response body. The last parameter to Execute()
 	// represents any dynamic data that we want to pass in, which for now we'll
 	// leave as nil.
 
-	err = ts.Execute(w, nil)
+	// Use the ExecuteTemplate method to write the content of the base template
+	// in the response body
+
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)

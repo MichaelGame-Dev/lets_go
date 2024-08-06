381249ed7a77d93b9a5ffcaded1f2a773b9f7ef1
Game Dev With Michael <44328815+MichaelGame-Dev@users.noreply.github.com>
2024-08-06 06:39:35 -0500

    added custom logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -14,8 +15,7 @@ func main() {
 
 	// of note, adding a flag like this lets you add -help and see it listed as an option.
 	// currently running `go run ./cmd/web -help` will show the addr flag as an option
-	// Another positive, if you set flags up, you can pass env variables into the command.
-	// ex. `go run ./cmd/web -addr=$SNIPPETBOX_ADDR`
+	// you could even call with env var: `go run ./cmd/web -addr=$SNIPPETBOX_ADDR`
 
 	// Importantly, we use the flag.Parse() function to parse the command-line flag.
 	// This reads in the command-line flag value and assigns it to the addr
@@ -23,6 +23,16 @@ func main() {
 	// otherwise it will always contain the default value of ":4000". If any errors are
 	// encountered during parsing the application will be terminated.
 	flag.Parse()
+
+	// Use the slog.New() function to initialize a new structured logger, which
+	// writes to the standard out stream and uses the default settings.
+	// Apparently there's also a JSON handler instead of text.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		// filename and line number
+		AddSource: true,
+		// Info is the default, added for easier changing later
+		Level: slog.LevelInfo,
+	}))
 
 	mux := http.NewServeMux()
 
@@ -42,15 +52,16 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	// The value returned from the flag.String() function is a pointer to the flag
-	// value, not the value itself. So in this code, that means the addr variable
-	// is actually a pointer, and we need to dereference it (i.e. prefix it with
-	// the * symbol) before using it. Note that we're using the log.Printf()
-	// function to interpolate the address with the log message.
-	log.Printf("starting server on %s", *addr)
+	// The value returned from the flag.String() function is a pointer, dereference
+	// with a leading *
+	// Use the Info() method to log the starting server message at Info severity
+	// (along with the listen address as an attribute).
+	logger.Info("starting server", "addr", *addr)
 
 	// TODO: add way to stop disable directory listings
 	// And we pass the dereferenced addr pointer to http.ListenAndServe() too.
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+
+	os.Exit(1)
 }
